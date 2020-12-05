40e166f5f3a870814e4df68e3ab794a14f5d849d
no_name <no@mail>
2020-12-05 14:53:12 +0100

    modifier le docker-compose.yml pour pull les images du hub. Suppression du Dockerfile qui était inutile.

--- scaleway-terraform/scaleway.tf
+++ scaleway-terraform/scaleway.tf
@@ -1,24 +1,56 @@
-provider "scaleway" {
-  organization_id = "46171436-216f-49d2-bfcd-130b98111056"
-  zone            = "fr-par-1"
-  region          = "fr-par"
+terraform {
+  required_providers {
+    scaleway = {
+      source = "scaleway/scaleway"
+      version = "1.17.2"
+    }
+  }
 }
 
-resource "scaleway_instance_ip" "public_ip" {}
+provider "scaleway" {
+  organization_id = "46171436-216f-49d2-bfcd-130b98111056"
+  zone = "fr-par-2"
+  region = "fr-par"
+}
+
+resource "scaleway_instance_ip" "public_ip" {
+}
 
 resource "scaleway_instance_volume" "data" {
-  size_in_gb = 550
+  size_in_gb = 30
   type = "l_ssd"
 }
 
-resource "scaleway_instance_server" "my-ubuntu-instance" {
-  type  = "GP1-M"
-  image = "ubuntu-focal"
+resource "scaleway_instance_security_group" "sg-devops-public" {
+  name = "sg-devops-public"
+  inbound_default_policy  = "drop"
+  outbound_default_policy = "accept"
+}
 
-  tags = [ "FocalFossa", "MyUbuntuInstance" ]
+data "scaleway_image" "docker" {
+  architecture = "x86_64"
+  name = "Docker"
+}
 
+
+resource "scaleway_instance_server" "scw-devops-project" {
+  type = "GP1-X"
+  image = "alpine-linux"
   ip_id = scaleway_instance_ip.public_ip.id
-
-  additional_volume_ids = [ scaleway_instance_volume.data.id ]
-
+  additional_volume_ids = [
+    scaleway_instance_volume.data.id]
+  provisioner "file" {
+    source = "docker-compose.yml"
+    destination = "/root/docker-compose.yml"
+  }
+  connection {
+    type = "ssh"
+    user = "root"
+    host = scaleway_instance_ip.public_ip.address
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "docker-compose up -d"
+    ]
+  }
 }
